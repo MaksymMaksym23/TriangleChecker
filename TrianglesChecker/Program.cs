04ba56a02f8ab01948e8036b9eaa8ee04574afa7
Maxmax231488 <sonic1488@gmail.com>
2018-03-12 20:02:57 +0200

    first commit

---
+++ TrianglesChecker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianglesChecker
+{
+    public enum TriangleTypes { Scalene, Isosceles, Equilateral, Error };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CheckTriangle: " + new Program().CheckTriangleType(2, 2, 2));
+            Console.ReadKey();
+        }
+
+        public TriangleTypes CheckTriangleType(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || a + c <= b)
+                return TriangleTypes.Error;
+
+            else if (a == b && b == c && c == a)
+                return TriangleTypes.Equilateral;
+
+            else if (a == b || b == c || c == a)
+                return TriangleTypes.Isosceles;
+
+            else if (a != b && b != c && a != c)
+                return TriangleTypes.Scalene;
+
+            else
+                return TriangleTypes.Error;
+        }
+    }
+}
---
+++ TrianglesChecker/TestProgram.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TrianglesChecker
+{
+    [TestFixture]
+    class TestProgram
+    {
+        [Test]
+        public void Test_Equilateral222()
+        {
+            Assert.AreEqual(TriangleTypes.Equilateral,
+                new Program().CheckTriangleType(2, 2, 2));
+        }
+
+        #region negative test cases
+        [Test]
+        public void Test_ErrorNegative()
+        {
+            Assert.AreEqual(TriangleTypes.Error,
+                new Program().CheckTriangleType(-3, 2, 4));
+        }
+
+        [Test]
+        public void Test_ErrorZero1()
+        {
+            Assert.AreEqual(TriangleTypes.Error,
+                new Program().CheckTriangleType(2, 0, 4));
+        }
+        [Test]
+        public void Test_ErrorZero2()
+        {
+            Assert.AreEqual(TriangleTypes.Error,
+                new Program().CheckTriangleType(2, 1, 0));
+        }
+
+        [Test]
+        public void Test_Error_C_isBigger()
+        {
+            Assert.AreEqual(TriangleTypes.Error,
+                new Program().CheckTriangleType(5, 5, 12));
+        }
+
+        [Test]
+        public void Test_Error_A_isBigger()
+        {
+            Assert.AreEqual(TriangleTypes.Error,
+                new Program().CheckTriangleType(11, 5, 4));
+        }
+
+        [Test]
+        public void Test_Error_B_Equal()
+        {
+            Assert.AreEqual(TriangleTypes.Error,
+                new Program().CheckTriangleType(11, 15, 4));
+        }
+        #endregion
+
+        #region Isosceles test cases
+        [Test]
+        public void Test_Isosceles_BA()
+        {
+            Assert.AreEqual(TriangleTypes.Isosceles,
+                new Program().CheckTriangleType(11, 11, 5));
+        }
+
+        [Test]
+        public void Test_Isosceles_CA()
+        {
+            Assert.AreEqual(TriangleTypes.Isosceles,
+                new Program().CheckTriangleType(5, 4, 5));
+        }
+
+        [Test]
+        public void Test_Isosceles_BC()
+        {
+            Assert.AreEqual(TriangleTypes.Isosceles,
+                new Program().CheckTriangleType(3, 5, 5));
+        }
+        #endregion
+
+        [Test]
+        public void Test_Scalene()
+        {
+            Assert.AreEqual(TriangleTypes.Scalene,
+                new Program().CheckTriangleType(3, 4, 2));
+        }
+    }
+}
